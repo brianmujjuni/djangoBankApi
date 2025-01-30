18325154b0fd21843d874896c8208e8c6798857a
brianmujjuni <brianmujjuni@engineer.com>
2025-01-31 00:27:45 +0300

    set up settings and add apps,password_hashers

---
+++ config/settings/local.py
@@ -0,0 +1,10 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-h5ro&yw3pt9kx*2)j*2kcu@$-(06u%a^zdf(crqv1#a+j$(7r@'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
