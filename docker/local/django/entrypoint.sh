bb60aefb96874cad9835f7e29bbbb448fef1a215
brianmujjuni <brianmujjuni@engineer.com>
2025-01-31 17:43:24 +0300

    add entrypoit to configure postgressql

---
+++ docker/local/django/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -0 errexit
+
+set -o pipefail
+
+set -o nounset
+
+python << END
+    import sys
+    import time
+    import psycopg2
+    suggest_unrecoverable_after = 30
+    start = time.time()
+    while True:
+    try:
+        psycopg2.connect(
+            dbname="${POSTGRES_DB}",
+            user="${POSTGRES_USER}",
+            password="${POSTGRES_PASSWORD}",
+            host="${POSTGRES_HOST}",
+            port="${POSTGRES_PORT}"
+        )
+        break
+    except psycopg2.OperationalError as error:
+        sys.stderr.write("Waiting for PostgresSql to become available...\n")
+        if time.time() - start > suggest_unrecoverable_after:
+            sys.stderr.write("PostgresSql is taking an unusually long time to become available. error: '{}'\n".format(error))
+            time.sleep(3)
+END
+
+echo >&2 'PostgresSql is available'
+
+exec "$@"
