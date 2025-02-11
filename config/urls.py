4d82748562468e49c96ebf4b34e24760f399454d
brianmujjuni <brianmujjuni@engineer.com>
2025-02-12 01:19:19 +0300

    customise admin dashboard

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,23 @@
-
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 
 urlpatterns = [
-    path('settings.ADMIN_URL', admin.site.urls),
-    
+    path("settings.ADMIN_URL", admin.site.urls),
+    path("api/v1/schema", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/vi/schema/swagger-ui",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+    ),
+    path("api/v1/schema/redoc",SpectacularRedocView.as_view(url_name="schema"),name="redoc")
 ]
+
+admin.site.site_header = "Automex Banck Admin"
+admin.site.site_title = "Automex Bank Admin Portal"
+admin.site.index_title = "Welcome to Automex Banck Admin Portal"
