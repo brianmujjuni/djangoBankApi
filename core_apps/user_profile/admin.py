38423a47758af056edef4531edd4642235d8c912
brianmujjuni <brianmujjuni@engineer.com>
2026-01-26 20:23:14 +0300

    add photo field to user profile admin

--- core_apps/user_profile/admin.py
+++ core_apps/user_profile/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from cloudinary.forms import CloudinaryFileField
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
+from .models import Profile, NextOfKin
 
 # Register your models here.
+class ProfileAdminForm(forms.ModelForm):
+    photo = CloudinaryFileField(
+        options={
+            "crop": "thumb",
+            "width": 200,
+            "height": 200,
+            "folder": "bank_photos",
+        },
+        required=False,
+    )
