ffda3b71db1e3d3982fa2262cbf20eb874099591
brianmujjuni <brianmujjuni@engineer.com>
2026-02-03 15:00:21 +0300

    create authenticate function in cookie auth file

--- core_apps/common/cookie_auth.py
+++ core_apps/common/cookie_auth.py
@@ -1,4 +1,4 @@
-from typing import Optional,Tuple
+from typing import Optional, Tuple
 from django.conf import settings
 from django.conf import settings
 from loguru import logger
@@ -6,3 +6,26 @@ from rest_framework.request import Request
 from rest_framework_simplejwt.authentication import JWTAuthentication, AuthUser
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.tokens import Token
+
+
+class CookieAuthentication(JWTAuthentication):
+    """
+    Custom authentication class that retrieves the JWT token from a cookie.
+    """
+
+    def authenticate(self, request: Request) -> Optional[Tuple[AuthUser, Token]]:
+        header = self.get_header(request)
+        raw_token = None
+
+        if header is not None:
+            raw_token = self.get_raw_token(header)
+        elif settings.COOKIE_NAME in request.COOKIES:
+            raw_token = request.COOKIES.get(settings.COOKIE_NAME)
+
+        if raw_token is not None:
+            try:
+                validated_token = self.get_validated_token(raw_token)
+                return self.get_user(validated_token), validated_token
+            except TokenError as e:
+                logger.error(f"Token validation error:{str(e)} ")
+            return None
