df4d0fce5e1ef8628216ff90d6fe8fc77c590251
brianmujjuni <brianmujjuni@engineer.com>
2026-02-05 00:15:15 +0300

    add auth urls

---
+++ core_apps/user_auth/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    CustomTokenCreateView,
+    CustomTokenRefreshView,
+    loggoutAPIView,
+    OTPVerifyView,
+)
+
+urlpatterns = [
+    path("/login", CustomTokenCreateView.as_view(), name="login"),
+    path("verify-otp/", OTPVerifyView.as_view(), name="verify_otp"),
+    path("refresh/", CustomTokenRefreshView.as_view(), name="refresh"),
+    path("logout/",loggoutAPIView.as_view(),name="logout")
+]
