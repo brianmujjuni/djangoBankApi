3d4bad7e246b4c98fcc2b85f8be8a04dc9a60d8a
brianmujjuni <brianmujjuni@engineer.com>
2026-02-04 21:51:08 +0300

    create helper function to generate otp for user

---
+++ core_apps/user_auth/views.py
@@ -0,0 +1,73 @@
+from typing import Any, Optional
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from djoser.views import TokenCreateView
+from djoser.views import User
+from loguru import logger
+from rest_framework import permissions, status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenRefreshView
+from .emails import send_otp_email
+from .utils import generate_otp_code
+
+
+User = get_user_model()
+
+
+def set_auth_cookies(
+    response: Response, access_token: str, refresh_token: Optional[str] = None
+) -> None:
+
+    access_token_lifetime = settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"].total_seconds()
+    cookie_settings = {
+        "path": settings.COOKIE_PATH,
+        "secure": settings.COOKIE_SECURE,
+        "httponly": settings.COOKIE_HTTPONLY,
+        "samesite": settings.COOKIE_SAMESITE,
+        "max_age": access_token_lifetime,
+    }
+    response.set_cookie("access", access_token, **cookie_settings)
+    if refresh_token:
+        refresh_token_lifetime = settings.SIMPLE_JWT[
+            "ACCESS_TOKEN_LIFETIME"
+        ].total_seconds()
+        refresh_cookie_settings = cookie_settings.copy()
+        refresh_cookie_settings["max_age"] = refresh_token_lifetime
+        response.set_cookie("refresh", refresh_token, **refresh_cookie_settings)
+
+    logged_in_cookie_settings = cookie_settings.copy()
+    logged_in_cookie_settings["httponly"] = False
+    response.set_cookie("logged_in", "true", **logged_in_cookie_settings)
+
+
+class CustomTokenCreateView(TokenCreateView):
+    def _action(self, serializer):
+        user = serializer.user
+        if user.is_locked_out:
+            return Response(
+                {
+                    "error": f"Account is locked due to multiple failed login attempts."
+                    f" Please try again later after. {settings.LOCKOUT_DURATION.total_seconds() / 60} minutes."
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        user.reset_failed_login_attempts()
+
+        otp = generate_otp_code()
+        user.set_opt(otp)
+        send_otp_email(user.email, otp)
+
+        logger.info(f"OTP sent to user {user.email} for login.")
+        return Response(
+            {
+                "success": "OTP has been sent to your email address.",
+                "email": user.email,
+            },
+            status=status.HTTP_200_OK,
+        )
+    
+    
