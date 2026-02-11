b22943552cf1527eedf0468436235b4d4e9fa2a9
brianmujjuni <brianmujjuni@engineer.com>
2026-02-12 00:09:06 +0300

    improve config urls

--- config/urls.py
+++ config/urls.py
@@ -10,19 +10,22 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-    path("api/v1/schema", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/v1/schema/swagger-ui",
+        "api/v1/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/v1/schema/redoc",
+        "api/v1/schema/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("core_apps.user_auth.urls")),
+    # path("api/v1/profiles/", include("core_apps.user_profile.urls")),
+    # path("api/v1/accounts/", include("core_apps.accounts.urls")),
+    # path("api/v1/cards/", include("core_apps.cards.urls")),
 ]
 
 admin.site.site_header = "Automex Bank Admin"
