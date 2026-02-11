c6c246841740742783de568aca5497f49aae6641
brianmujjuni <brianmujjuni@engineer.com>
2026-02-11 23:45:53 +0300

    comment url paths

--- config/urls.py
+++ config/urls.py
@@ -1,25 +1,30 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
 
- 
+
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     path("api/v1/schema", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/v1/schema/swagger-ui",
         SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui"
+        name="swagger-ui",
     ),
-    path("api/v1/schema/redoc",SpectacularRedocView.as_view(url_name="schema"),name="redoc")
+    path(
+        "api/v1/schema/redoc",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    # path("api/v1/auth/", include("djoser.urls")),
+    # path("api/v1/auth", include("core_apps.user_auth.urls")),
 ]
 
 admin.site.site_header = "Automex Bank Admin"
 admin.site.site_title = "Automex Bank Admin Portal"
 admin.site.index_title = "Welcome to Automex Bank Admin Portal"
-
